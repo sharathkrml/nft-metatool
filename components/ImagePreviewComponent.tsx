9dec1cb7c7617b9668dce0a79160df2c9576260f
Sharath <sharathkrml@gmail.com>
2022-06-10 12:33:09 +0530

    fixed a typo

--- components/ImagePreviewComponent.tsx
+++ components/ImagePreviewComponent.tsx
@@ -6,7 +6,7 @@ function ImagePreviewComponent() {
             <div className="flex rounded-t-md justify-end items-center py-2 px-2 bg-[#303339]">
                 <Favorite className="text-base text-[#8A939B]" /><span className="ml-1 text-base">0</span>
             </div>
-            <div className="w-h-60 h-60 rounded-b-md relative bg-red-100">
+            <div className="w-auto h-80 rounded-b-md relative bg-red-100">
                 <Image src='/0.png' layout='fill'
                     objectFit='contain' alt="preview" />
             </div>
