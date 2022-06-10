70e01296c6adb4cb2ad4769b1fb4c5aeea209fab
Sharath <sharathkrml@gmail.com>
2022-06-10 12:19:31 +0530

    added image component

--- components/DisplayComponent.tsx
+++ components/DisplayComponent.tsx
@@ -1,10 +1,12 @@
 import ExternalLinkComponent from "./ExternalLinkComponent";
+import ImagePreviewComponent from "./ImagePreviewComponent";
 
 const DisplayComponent = () => {
     return (
         <div className="text-white px-10 py-5">
             <ExternalLinkComponent />
-            <div className="text-4xl text-[#E5E8EB]">NFT NAME #</div>
+            <div className="mt-3 mb-5 text-4xl text-[#E5E8EB]">NFT NAME #</div>
+            <ImagePreviewComponent/>
         </div>
     );
 };
---
+++ components/ImagePreviewComponent.tsx
@@ -0,0 +1,17 @@
+import { Favorite } from "@mui/icons-material"
+import Image from "next/image"
+function ImagePreviewComponent() {
+    return (
+        <div className=''>
+            <div className="flex rounded-t-md justify-end items-center py-2 px-2 bg-[#303339]">
+                <Favorite className="text-base text-[#8A939B]" /><span className="ml-1 text-base">0</span>
+            </div>
+            <div className="w-h-60 h-60 rounded-b-md relative bg-red-100">
+                <Image src='/0.png' layout='fill'
+                    objectFit='contain' alt="preview" />
+            </div>
+        </div>
+    )
+}
+
+export default ImagePreviewComponent
